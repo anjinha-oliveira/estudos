6eccb3c7d9f698566c04c2c13247591c92f4c897
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-03-20 21:53:53 -0300

    Aula sobre box-level e inline-level

--- python/calculadora.py
+++ python/calculadora.py
@@ -1,9 +1,9 @@
-number = int(input('Digite o número para fazer a tabuada: '))
+numero = int(input('Digite o número para fazer a tabuada: '))
 print(f'***---***'*3)
 print(f'Segue abaixo a tabuada')
 print(f'***---***'*3)
 for i in range(1,11):
-    tabuada = (number * i)
-    print(f'{i} x {number} = {tabuada}')
+    tabuada = (numero * i)
+    print(f'{numero} x {i} = {tabuada}')
 
 
